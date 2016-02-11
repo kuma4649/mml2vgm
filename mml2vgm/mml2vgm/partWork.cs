a8d2eb846e25aac0db2cc22a8a4097b3f0c3a115
kuma <uta@mymelody.jp>
2016-02-11 10:43:39 +0900

    コメント追加

--- mml2vgm/mml2vgm/partWork.cs
+++ mml2vgm/mml2vgm/partWork.cs
@@ -27,7 +27,7 @@ namespace mml2vgm
         /// <summary>
         /// 次に演奏されるデータの位置
         /// </summary>
-        private clsPos pos=new clsPos();
+        private clsPos pos = new clsPos();
 
         /// <summary>
         /// 位置情報のスタック
@@ -49,8 +49,14 @@ namespace mml2vgm
         /// </summary>
         public int ch = 0;
 
+        /// <summary>
+        /// 割り当てられたパート名
+        /// </summary>
         public string partName = "";
 
+        /// <summary>
+        /// 未加工のf-num
+        /// </summary>
         public int freq = 0;
 
         /// <summary>
@@ -94,14 +100,29 @@ namespace mml2vgm
         /// </summary>
         public int instrument = -1;
 
+        /// <summary>
+        /// エンベロープの進捗位置
+        /// </summary>
         public int envIndex = -1;
 
+        /// <summary>
+        /// エンベロープ向け汎用カウンター
+        /// </summary>
         public int envCounter = -1;
 
+        /// <summary>
+        /// エンベロープ音量
+        /// </summary>
         public int envVolume = -1;
 
-        public int[] envelope = new int[8] { 0,0,0,0,0,0,0,0 };
+        /// <summary>
+        /// 使用中のエンベロープの定義
+        /// </summary>
+        public int[] envelope = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
 
+        /// <summary>
+        /// エンベロープスイッチ
+        /// </summary>
         public bool envelopeMode = false;
 
         /// <summary>
@@ -109,10 +130,19 @@ namespace mml2vgm
         /// </summary>
         public int detune = 0;
 
+        /// <summary>
+        /// 発音される音程
+        /// </summary>
         public char noteCmd = 'c';
 
+        /// <summary>
+        /// 音程をずらす量
+        /// </summary>
         public int shift = 0;
 
+        /// <summary>
+        /// PCMの音程
+        /// </summary>
         public int pcmNote = 0;
 
         /// <summary>
@@ -135,10 +165,19 @@ namespace mml2vgm
         /// </summary>
         public byte slots = 0xf;
 
+        /// <summary>
+        /// タイ
+        /// </summary>
         public bool tie = false;
 
+        /// <summary>
+        /// 前回発音時にタイ指定があったかどうか
+        /// </summary>
         public bool beforeTie = false;
 
+        /// <summary>
+        /// 前回発音時の音量
+        /// </summary>
         public int beforeVolume = -1;
 
         /// <summary>
@@ -152,7 +191,7 @@ namespace mml2vgm
         public bool Ch3SpecialModeKeyOn = false;
 
         /// <summary>
-        /// Lfo
+        /// Lfo(4つ)
         /// </summary>
         public clsLfo[] lfo = new clsLfo[4] { new clsLfo(), new clsLfo(), new clsLfo(), new clsLfo() };
 
@@ -161,35 +200,70 @@ namespace mml2vgm
         /// </summary>
         public int bendOctave = 4;
 
+        /// <summary>
+        /// ベンド中の音程
+        /// </summary>
         public char bendNote = 'r';
 
+        /// <summary>
+        /// ベンド中の待機カウンター
+        /// </summary>
         public long bendWaitCounter = -1;
 
+        /// <summary>
+        /// ベンド中に参照される周波数スタックリスト
+        /// </summary>
         public Stack<Tuple<int, int>> bendList = new Stack<Tuple<int, int>>();
 
+        /// <summary>
+        /// ベンド中の発音周波数
+        /// </summary>
         public int bendFnum = 0;
 
+        /// <summary>
+        /// ベンド中に音程をずらす量
+        /// </summary>
         public int bendShift = 0;
 
+        /// <summary>
+        /// スロットごとのディチューン値
+        /// </summary>
         public int[] slotDetune = new int[] { 0, 0, 0, 0 };
 
+        /// <summary>
+        /// ノイズモード値
+        /// </summary>
         public int noise = 0;
 
+        /// <summary>
+        /// キーシフト
+        /// </summary>
         public int keyShift = 0;
 
 
 
+        /// <summary>
+        /// パート情報をリセットする
+        /// </summary>
         public void resetPos()
         {
             pos = new clsPos();
             stackPos = new Stack<clsPos>();
         }
 
+        /// <summary>
+        /// 解析位置を取得する
+        /// </summary>
+        /// <returns></returns>
         public int getPos()
         {
             return pos.tCol;
         }
 
+        /// <summary>
+        /// 解析位置に対するソースファイル上の行数を得る
+        /// </summary>
+        /// <returns></returns>
         public int getLineNumber()
         {
             if (pos.alies == "")
@@ -199,6 +273,10 @@ namespace mml2vgm
             return aData[pos.alies].Item1;
         }
 
+        /// <summary>
+        /// 解析位置の文字を取得する
+        /// </summary>
+        /// <returns></returns>
         public char getChar()
         {
             char ch;
@@ -214,16 +292,27 @@ namespace mml2vgm
             return ch;
         }
 
+        /// <summary>
+        /// 解析位置を一つ進める(重い！)
+        /// </summary>
         public void incPos()
         {
             setPos(pos.tCol + 1);
         }
 
+        /// <summary>
+        /// 解析位置を一つ戻す(重い！)
+        /// </summary>
         public void decPos()
         {
             setPos(pos.tCol - 1);
         }
 
+        /// <summary>
+        /// 指定された文字数だけ読み出し、文字列を生成する
+        /// </summary>
+        /// <param name="len">文字数</param>
+        /// <returns>文字列</returns>
         public string getString(int len)
         {
             StringBuilder sb = new StringBuilder();
@@ -235,6 +324,11 @@ namespace mml2vgm
             return sb.ToString();
         }
 
+        /// <summary>
+        /// 解析位置を指定する
+        /// </summary>
+        /// <param name="tCol">解析位置</param>
+        /// <remarks>エイリアスを毎回展開しながら位置を算出するためとても効率が悪い</remarks>
         public void setPos(int tCol)
         {
             if (pData == null)
@@ -381,30 +475,40 @@ namespace mml2vgm
 
         }
 
+        /// <summary>
+        /// 解析位置から数値を取得する。
+        /// </summary>
+        /// <param name="num">取得した数値が返却される</param>
+        /// <returns>数値取得成功したかどうか</returns>
         public bool getNum(out int num)
         {
 
             string n = "";
             int ret = -1;
 
+            //タブと空白は読み飛ばす
             while (getChar() == ' ' || getChar() == '\t')
             {
                 incPos();
             }
 
+            //符号を取得する(ない場合は正とする)
             if (getChar() == '-' || getChar() == '+')
             {
                 n = getChar().ToString();
                 incPos();
             }
 
+            //タブと空白は読み飛ばす
             while (getChar() == ' ' || getChar() == '\t')
             {
                 incPos();
             }
 
+            //１６進数指定されているか
             if (getChar() != '$')
             {
+                //数字でなくなるまで取得
                 while (true)
                 {
                     if (getChar() >= '0' && getChar() <= '9')
@@ -425,6 +529,7 @@ namespace mml2vgm
                     }
                 }
 
+                //数値に変換できたら成功
                 if (!int.TryParse(n, out ret))
                 {
                     num = -1;
@@ -435,12 +540,15 @@ namespace mml2vgm
             }
             else
             {
+                //２文字取得
                 incPos();
                 n += getChar();
                 incPos();
                 n += getChar();
                 incPos();
-                try {
+                //数値に変換できたら成功
+                try
+                {
                     num = Convert.ToInt32(n, 16);
                 }
                 catch
@@ -453,6 +561,12 @@ namespace mml2vgm
             return true;
         }
 
+        /// <summary>
+        /// エイリアス名を取得する
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
         private string getAliesName(string data, int col)
         {
             if (data.Length <= col + 1)
@@ -475,12 +589,12 @@ namespace mml2vgm
 
     public enum ePartType
     {
-        YM2612 , YM2612extend , SegaPSG
+        YM2612, YM2612extend, SegaPSG
     }
 
     public enum eLfoType
     {
-        Tremolo , Vibrato , Hardware
+        Tremolo, Vibrato, Hardware
     }
 
     public class clsPos
@@ -524,12 +638,39 @@ namespace mml2vgm
     public class clsLfo
     {
 
+        /// <summary>
+        /// Lfoの種類
+        /// </summary>
         public eLfoType type = eLfoType.Hardware;
-        public List<int> param =null;
+
+        /// <summary>
+        /// Lfoの設定値
+        /// </summary>
+        public List<int> param = null;
+
+        /// <summary>
+        /// Lfoのスイッチ
+        /// </summary>
         public bool sw = false;
+
+        /// <summary>
+        /// Lfoが完了したかどうか
+        /// </summary>
         public bool isEnd = false;
+
+        /// <summary>
+        /// Lfoの待機カウンター
+        /// </summary>
         public long waitCounter = 0;
+
+        /// <summary>
+        /// Lfoの変化値
+        /// </summary>
         public int value = 0;
+        
+        /// <summary>
+        /// Lfoの変化する方向
+        /// </summary>
         public int direction = 0;
 
     }
